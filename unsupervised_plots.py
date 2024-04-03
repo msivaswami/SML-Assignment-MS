b9ed67ee09bad351f93f53bab64e73cb2457ad24
Muthukumaraswami Sivaswami <147249311+msivaswami@users.noreply.github.com>
2024-04-03 09:47:41 +0400

    Add files via upload
    Changes to script to add microarray

--- unsupervised_plots.py
+++ unsupervised_plots.py
@@ -8,27 +8,31 @@ import matplotlib.image as mpimg
 img1 = mpimg.imread('pca_log2FPKM.png')
 img2 = mpimg.imread('kmeans_log2FPKM.png')
 img3 = mpimg.imread('tsne2.png')
+img4 = mpimg.imread('pca_microarray.jpeg')
 
 #Creating a figure and a set of subplots
-fig, axs = plt.subplots(1, 3, figsize=(15, 5))
+fig, axs = plt.subplots(2, 2, figsize=(10, 10))
 
 #Removing axes
-for ax in axs:
-    ax.axis('off')
+for ax_row in axs:
+    for ax in ax_row:
+        ax.axis('off')
 
 #Plotting the images
-axs[0].imshow(img1)
-axs[0].text(0.5, -0.1, 'A', size=12, ha='center', transform=axs[0].transAxes, weight='bold')
-axs[1].imshow(img2)
-axs[1].text(0.5, -0.1, 'B', size=12, ha='center', transform=axs[1].transAxes, weight='bold')
-axs[2].imshow(img3)
-axs[2].text(0.5, -0.1, 'C', size=12, ha='center', transform=axs[2].transAxes, weight='bold')
+axs[0, 0].imshow(img1)
+axs[0, 0].text(0.5, -0.1, 'A', size=12, ha='center', transform=axs[0, 0].transAxes, weight='bold')
+axs[0, 1].imshow(img2)
+axs[0, 1].text(0.5, -0.1, 'B', size=12, ha='center', transform=axs[0, 1].transAxes, weight='bold')
+axs[1, 0].imshow(img3)
+axs[1, 0].text(0.5, -0.1, 'C', size=12, ha='center', transform=axs[1, 0].transAxes, weight='bold')
+axs[1, 1].imshow(img4)
+axs[1, 1].text(0.5, -0.1, 'D', size=12, ha='center', transform=axs[1, 1].transAxes, weight='bold')
 
 #Adjusting spacing between subplots
-plt.subplots_adjust(wspace=0.1)
+plt.subplots_adjust(wspace=0.01, hspace=0.01)
 
 #Saving the plot
-plt.savefig('gridplot_labels.png')
+plt.savefig('2x2_gridplot_labels_final.png')
 
 plt.tight_layout()
 plt.show()
